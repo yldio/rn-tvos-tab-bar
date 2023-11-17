c05d5136b8d9c7be16e6710e1a0459b305f9f6cc
Mathis Pierron <mathis.pierron@student-cs.fr>
2023-11-17 13:08:04 +0000

    Add Fabric example project

---
+++ FabricTvOSExample/App.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { TabBar } from 'rn-tvos-tab-bar/components/TabBar';
+
+function App(): JSX.Element {
+  const currentIndex = React.useRef(1);
+  const [items, setItems] = React.useState(['Item 1']);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  return (
+    <View style={styles.container}>
+      <TabBar
+        items={items}
+        selectedItem={selectedIndex}
+        style={styles.tabBar}
+        onSelectItem={({index}) => {
+          setSelectedIndex(index);
+        }}
+      />
+      <Button
+        onPress={() => {
+          setItems(_items => [..._items, `Item ${++currentIndex.current}`]);
+        }}
+        title="Add an item"
+      />
+      <Text style={styles.text}>Selected item: {items[selectedIndex]}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    gap: 50,
+  },
+  tabBar: {
+    width: '100%',
+    height: 70,
+    marginTop: 20,
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
