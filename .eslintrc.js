98c68a6c6f6bbb5387601c9857cc929399beede9
Mathis Pierron <mathis.pierron2@gmail.com>
2023-11-22 09:50:02 +0000

    feat: add linter and formatter

---
+++ .eslintrc.js
@@ -0,0 +1,104 @@
+module.exports = {
+  extends: [
+    "standard",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:react/recommended",
+    "prettier",
+  ],
+
+  plugins: ["react-native", "react-hooks"],
+
+  env: {
+    "react-native/react-native": true,
+  },
+
+  settings: {
+    "import/ignore": [
+      "node_modules/react-native/index\\.js$",
+      "react-native/Libraries/Utilities/codegenNativeComponent.*",
+      "react-native/Libraries/Types/CodegenTypes.*",
+    ],
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".native.js"],
+      },
+    },
+    react: {
+      version: "detect",
+    },
+  },
+
+  rules: {
+    "react/prop-types": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+
+  overrides: [
+    {
+      files: ["*.{js,jsx}"],
+      parser: "babel-eslint",
+    },
+    {
+      files: ["*.{ts,tsx}"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+      extends: ["plugin:@typescript-eslint/recommended"],
+      settings: {
+        "import/parsers": {
+          "@typescript-eslint/parser": [".ts", ".tsx"],
+        },
+      },
+      rules: {
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/array-type": "error",
+        "@typescript-eslint/consistent-type-assertions": [
+          "error",
+          { assertionStyle: "as" },
+        ],
+        "@typescript-eslint/member-delimiter-style": "error",
+        "@typescript-eslint/no-dynamic-delete": "error",
+        "@typescript-eslint/no-empty-interface": [
+          "error",
+          { allowSingleExtends: true },
+        ],
+        "@typescript-eslint/no-extraneous-class": "error",
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          { argsIgnorePattern: "^_" },
+        ],
+        "@typescript-eslint/no-use-before-define": [
+          "error",
+          {
+            functions: false,
+            classes: false,
+            variables: false,
+            typedefs: false,
+          },
+        ],
+        "@typescript-eslint/no-useless-constructor": "error",
+        "@typescript-eslint/prefer-for-of": "error",
+        "@typescript-eslint/prefer-function-type": "error",
+        "@typescript-eslint/prefer-optional-chain": "error",
+        "@typescript-eslint/unified-signatures": "error",
+
+        "default-case": "off",
+        "no-unused-vars": "off",
+        "no-array-constructor": "off",
+        "no-use-before-define": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/ban-ts-comment": [
+          "error",
+          {
+            "ts-ignore": "allow-with-description",
+            "ts-expect-error": "allow-with-description",
+          },
+        ],
+      },
+    },
+  ],
+};
